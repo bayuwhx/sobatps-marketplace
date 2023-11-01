7127b05267d1e6a563c0b87f25a1ceac6bebb6be
@bayuwhx <bayuharimurti2@gmail.com>
2023-11-01 19:19:05 +0700

    fixing logo route

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar shadow-lg navbar-expand-lg bg-body-tertiary">
     <div class="container">
-        <a class="navbar-brand" href="#"><img src="/img/sobat-ps.png" alt="..." height="18"></a>
+        <a class="navbar-brand" href="/"><img src="/img/sobat-ps.png" alt="..." height="18"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
